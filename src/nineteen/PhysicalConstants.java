a996bbea62c11c456989a7005966130dc2737a7e
nasus <turodog@foxmail.com>
2017-08-27 22:51:03 +0800

    接口只用于定义类型

---
+++ src/nineteen/PhysicalConstants.java
@@ -0,0 +1,15 @@
+package nineteen;
+
+public class PhysicalConstants {
+	private PhysicalConstants() {
+	} // Prevents instantiation·ÀÖ¹ÊµÀý»¯
+
+	// Avogadro's number (1/mol)
+	public static final double AVOGADROS_NUMBER = 6.02214199e23;
+
+	// Boltzmann constant (J/K)
+	public static final double BOLTZMANN_CONSTANT = 1.3806503e-23;
+
+	// Mass of the electron (kg)
+	public static final double ELECTRON_MASS = 9.10938188e-31;
+}
