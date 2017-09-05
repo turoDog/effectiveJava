bc72ec2c8bc14b35151372508061fa97dc4caaa3
nasus <turodog@foxmail.com>
2017-09-05 21:31:18 +0800

    for-each循环优先于传统的for循环

---
+++ src/item_46/DiceRolls.java
@@ -0,0 +1,26 @@
+package item_46;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+enum Face{
+	ONE, TWO, ThREE, FOUR, FIVE, SIX
+}
+
+public class DiceRolls {
+
+	public static void main(String[] args) {
+		Collection<Face> faces = Arrays.asList(Face.values());
+		
+		//for (Iterator<Face> i = faces.iterator(); i.hasNext();)
+			//for (Iterator<Face> j = faces.iterator(); j.hasNext();)
+				//System.out.println(i.next() + " " + j.next());
+		
+		// Preferred idiom for nested iteration on collections and arrays
+		 for (Face face1 : faces)
+		 for (Face face2 : faces)
+		 System.out.println(face1 + " " + face2);
+	}
+
+}
