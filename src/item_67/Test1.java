5ea0d8c56c04f6460de78978f39fa94c3f9c728d
nasus <turodog@foxmail.com>
2017-09-12 22:54:58 +0800

    避免过度同步

---
+++ src/item_67/Test1.java
@@ -0,0 +1,19 @@
+package item_67;
+
+import java.util.HashSet;
+
+public class Test1 {
+	public static void main(String[] args) {
+		ObservableSet<Integer> set = new ObservableSet<Integer>(
+				new HashSet<Integer>());
+
+		set.addObserver(new SetObserver<Integer>() {
+			public void added(ObservableSet<Integer> s, Integer e) {
+				System.out.println(e);
+			}
+		});
+
+		for (int i = 0; i < 100; i++)
+			set.add(i);
+	}
+}
---
+++ src/item_67/Test2.java
@@ -0,0 +1,21 @@
+package item_67;
+
+import java.util.HashSet;
+
+public class Test2 {
+	public static void main(String[] args) {
+		ObservableSet<Integer> set = new ObservableSet<Integer>(
+				new HashSet<Integer>());
+
+		set.addObserver(new SetObserver<Integer>() {
+			public void added(ObservableSet<Integer> s, Integer e) {
+				System.out.println(e);
+				if (e == 23)
+					s.removeObserver(this);
+			}
+		});
+
+		for (int i = 0; i < 100; i++)
+			set.add(i);
+	}
+}
